ad55d379b91ca6b9dd6ad4928e58bcd04806c8db
Gary Mannion <gkmannion@hotmail.com>
2017-02-28 22:08:53 +0000

    design updates

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController, AlertController} from 'ionic-angular';
-
+// import of angular fire and firebase from angularfire2
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
@@ -12,15 +12,17 @@ import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 export class HomePage {
     trips: FirebaseListObservable<any>;
-
+// constructor for what database to call and where from
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, angFire: AngularFire) {
     this.trips = angFire.database.list('/Trips');
-    
   }
   
+  // addTrip method
   addTrip():void {
+      // prompt to create
     let prompt = this.alertCtrl.create({
         title: 'Enter Trip Details',
+        // all the details to input into user trips
         inputs: [
             {
                 name: 'name',
@@ -45,35 +47,40 @@ export class HomePage {
             {
                 name: 'activities',
                 placeholder: "Activities"
+            },
+            {
+                name: 'passengers',
+                placeholder: "Passenger"
             }
         ],
+        // buttons at the end of the form (Cancel, Create)
         buttons: [
             {
-                text: "Cancel",
-                handler: data => {
-                    console.log("Cancel clicked");
-                }
-            },
+                text: "Cancel"
+            }, // end Cancel
             {
                 text: "Create Trip",
                 handler: data => {
+                    // adds the data to the my trips 
                     this.trips.push({
                         name: data.name,
                         Number: data.Number,
                         hotelName: data.hotelName,
                         hotelAdd: data.hotelAdd,
                         transfer: data.transfer,
-                        activities: data.activities
-                    })
-                }
-            }
-        ]
-    });
+                        activities: data.activities,
+                        passengers: data.passengers
+                    }) // end push
+                } // end handler
+            } // end create
+        ] // end buttons
+    }); // end create prompt 
     
-    prompt.present();
+    prompt.present(); // presents the prompt request
     
-  }
+  } // end addTrip
   
+  // editTrip. Calls trip database object
   editTrip(trip):void {
     let prompt = this.alertCtrl.create({
         title: 'Edit trip',
@@ -101,25 +108,29 @@ export class HomePage {
             {
                 name: 'activities',
                 placeholder: trip.activities
+            },
+            {
+                name: 'passengers',
+                placeholder: trip.passengers
             }
         ],
         buttons: [
             {
                 text: "Cancel",
-                handler: data => {
-                    console.log("Cancel clicked");
-                }
             },
             {
                 text: "Edit Trip",
                 handler: data => {
+                    // tells app to let new details entered to replace existing
                     let newName:String = trip.name;
                     let newNumber:String = trip.Number;
                     let newHotelName:String = trip.hotelName;
                     let newHotelAdd:String = trip.hotelAdd;
                     let newTransfer:String = trip.transfer;
                     let newActivities:String = trip.activities;
+                    let newPassengers:String = trip.passengers;
                     
+                    // tells app that if no new value entered leave existing detail there
                     if(data.name !=''){
                         newName = data.name;
                     }
@@ -143,44 +154,47 @@ export class HomePage {
                      if(data.activities !=''){
                         newActivities = data.activities;
                     }
+                    if(data.passengers != ''){
+                        newPassengers = data.passengers;
+                    }
                     
+                    // returns the updated values entered by a user
                     this.trips.update(trip.$key,{
                         name: newName,
                         Number: newNumber,
                         hotelName: newHotelName,
                         hotelAdd: newHotelAdd,
                         transfer: newTransfer,
-                        activities: newActivities
-                    })
-                }
-            }
-        ]
-    });
+                        activities: newActivities,
+                        passengers: newPassengers
+                    }) // end update
+                } // end handler
+            } // end Edit
+        ] // end buttons
+    }); // end prompt
     
-    prompt.present();
-}
+    prompt.present(); // presents the prompt request
+} // end editTrip
     
+    // deleteTrip. Call tripID from trip.$key
     deleteTrip(tripID):void {
     let prompt = this.alertCtrl.create({
         title: 'Delete trip',
         buttons: [
             {
-                text: "Cancel",
-                handler: data => {
-                    console.log("Cancel clicked");
-                }
+                text: "Cancel"
             },
             {
                 text: "Delete Trip",
                 handler: data => {
                     this.trips.remove(tripID)
-                }
-            }
-        ]
-    });
+                } // end handler to remove trip
+            } // end text
+        ] // end buttons
+    }); // end prompt
     
-    prompt.present();
+    prompt.present(); // presents the prompt request
     
-  }
+  } // end deleteTrip
 
-}
+} // end main
